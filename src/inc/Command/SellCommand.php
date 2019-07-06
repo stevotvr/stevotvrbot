963299a6d499ef3f0e44c892448af25dfc0689f4
Steve Guidetti <stevotvr@gmail.com>
2019-07-06 14:59:31 -0700

    Use the StreamElements API to give points to users

--- src/inc/Command/SellCommand.php
+++ src/inc/Command/SellCommand.php
@@ -10,6 +10,7 @@
 
 namespace StevoTVRBot\Command;
 
+use StevoTVRBot\Config;
 use StevoTVRBot\Model\ItemsModel;
 
 /**
@@ -44,11 +45,55 @@ class SellCommand extends Command
  		$sold = ItemsModel::sell($user, $args);
  		if ($sold)
  		{
-		    printf('!addpoints %s %d', $sold['user'], $sold['value']);
+ 			if ($this->addUserPoints($user, $sold['value']))
+ 			{
+		    	printf('%s sold %s for $%d', $sold['user'], $args, $sold['value']);
+ 			}
+ 			else
+ 			{
+		        header($_SERVER['SERVER_PROTOCOL'] . ' 503 Service Unavailable');
+		        echo '503 Service Unavailable';
+ 			}
  		}
  		else
  		{
             printf('%s, that item could not be found in your inventory.', $user);
  		}
+	}
+
+	/**
+	 * Add points to a StreamElements user.
+	 *
+	 * @param string $user   The username of the user
+	 * @param int    $points The number of points to add
+	 *
+	 * @return boolean True if the API request was successful, otherwise false
+	 */
+	private function addUserPoints(string $user, int $points)
+	{
+		$ctx = stream_context_create([
+			'http'	=> [
+				'ignore_errors'	=> '1',
+				'method'		=> 'PUT',
+				'header'		=> [
+					'Accept: application/json',
+					'Content-Type: Content-Type',
+					'Authorization: Bearer ' . Config::SE_JWT_TOKEN,
+				],
+			],
+		]);
+		$url = sprintf('https://api.streamelements.com/kappa/v2/points/%s/%s/%d', Config::SE_CHANNEL_ID, $user, $points);
+		$stream = @fopen($url, 'r', false, $ctx);
+		if (!$stream)
+		{
+			return false;
+		}
+
+		$meta = stream_get_meta_data($stream);
+		$status = array_shift($meta['wrapper_data']);
+		$response_code = (int) substr($status, strpos($status, ' ') + 1, 3);
+		fclose($stream);
+
+		return $response_code === 200;
 	}
 }

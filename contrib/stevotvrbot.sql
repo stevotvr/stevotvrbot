82944943504f7fe320cd86015d8177d92c5e7421
Steve Guidetti <stevotvr@gmail.com>
2019-10-18 11:40:18 -0700

    Update database schema

--- contrib/stevotvrbot.sql
+++ contrib/stevotvrbot.sql
@@ -17,31 +17,6 @@ CREATE TABLE `commands` (
   `level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `inventory` (
-  `id` int(11) NOT NULL,
-  `user` varchar(32) NOT NULL,
-  `item` int(11) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `items` (
-  `id` int(11) NOT NULL,
-  `slug` varchar(64) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `nameSingle` varchar(64) NOT NULL,
-  `namePlural` varchar(64) NOT NULL,
-  `value` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL DEFAULT '0',
-  `weight` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `recipe` (
-  `id` int(11) NOT NULL,
-  `item` int(11) NOT NULL,
-  `ingredient` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 CREATE TABLE `schedule` (
   `id` int(11) NOT NULL,
   `day` int(11) NOT NULL,
@@ -63,7 +38,7 @@ CREATE TABLE `tips` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `user` varchar(32) NOT NULL,
   `message` text NOT NULL,
-  `status` enum('PENDING','APPROVED','REJECTED','') NOT NULL DEFAULT 'PENDING'
+  `status` enum('PENDING','APPROVED','REJECTED') NOT NULL DEFAULT 'PENDING'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `users` (
@@ -78,24 +53,6 @@ CREATE TABLE `users` (
 ALTER TABLE `commands`
   ADD PRIMARY KEY (`id`),
   ADD KEY `command` (`command`);
-
-ALTER TABLE `inventory`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user` (`user`),
-  ADD KEY `item` (`item`);
-
-ALTER TABLE `items`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`) USING BTREE,
-  ADD UNIQUE KEY `nameSingle` (`nameSingle`),
-  ADD UNIQUE KEY `namePlural` (`namePlural`),
-  ADD UNIQUE KEY `slug` (`slug`),
-  ADD KEY `weight` (`weight`);
-
-ALTER TABLE `recipe`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `item` (`item`),
-  ADD KEY `ingredient` (`ingredient`);
 
 ALTER TABLE `schedule`
   ADD PRIMARY KEY (`id`);
@@ -117,15 +74,6 @@ ALTER TABLE `users`
 ALTER TABLE `commands`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `inventory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `recipe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
 ALTER TABLE `schedule`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
@@ -134,14 +82,6 @@ ALTER TABLE `tips`
 
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-
-ALTER TABLE `inventory`
-  ADD CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`item`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
-
-ALTER TABLE `recipe`
-  ADD CONSTRAINT `recipe_ibfk_1` FOREIGN KEY (`item`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `recipe_ibfk_2` FOREIGN KEY (`ingredient`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

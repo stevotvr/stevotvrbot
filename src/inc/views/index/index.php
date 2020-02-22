cb3d3cedb056217c98aa9426b4837497cdcf0dfe
Steve Guidetti <stevotvr@gmail.com>
2020-02-22 15:58:49 -0800

    Add Mixer player to the front page

--- src/inc/views/index/index.php
+++ src/inc/views/index/index.php
@@ -4,12 +4,15 @@
 		</header>
 		<section id="twitch-embed">
 		</section>
+		<section>
+			<iframe src="https://mixer.com/embed/player/stevotvr?muted=1" width="100%" height="300"></iframe>
+		</section>
 	</section>
 	<script src="https://embed.twitch.tv/embed/v1.js"></script>
 	<script type="text/javascript">
       new Twitch.Embed("twitch-embed", {
         width: '100%',
-        height: 720,
+        height: 500,
         channel: "stevotvr"
       });
     </script>
